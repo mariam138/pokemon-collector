854523fe6cb8fda6ea11d747e8a0d2df46b02c8b
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2026-02-26 10:06:02 +0000

    Updating CORS

--- backend/src/main/java/com/example/pokemon/config/SecurityConfig.java
+++ backend/src/main/java/com/example/pokemon/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173/",
+        config.setAllowedOrigins(List.of("http://localhost:*",
                 "https://pokemon-collector-frontend-production.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
