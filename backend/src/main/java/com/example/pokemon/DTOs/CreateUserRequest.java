07b1de15c5c30e380d65b81d9cacc7e5556d3fb5
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-26 12:28:49 +0000

    add pattern checks for name and email

--- backend/src/main/java/com/example/pokemon/DTOs/CreateUserRequest.java
+++ backend/src/main/java/com/example/pokemon/DTOs/CreateUserRequest.java
@@ -6,11 +6,20 @@ public class CreateUserRequest {
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 100, message =
             "Name must be between 3 and 100 char long")
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$",
+            message = "Name contains invalid characters"
+    )
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,63}$",
+            message = "Email must be a valid format (example@example.com)"
+    )
     private String email;
+
 
     public CreateUserRequest() {
     }
