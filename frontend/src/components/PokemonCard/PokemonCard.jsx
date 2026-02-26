4e0ff19d1668492b9a36b367ebdb7b7a424b7fa8
Remi HOEPPE <remihoeppe@users.noreply.github.com>
2026-02-26 16:02:34 +0000

    Adding checkbox logic on frontend

--- frontend/src/components/PokemonCard/PokemonCard.jsx
+++ frontend/src/components/PokemonCard/PokemonCard.jsx
@@ -1,85 +1,68 @@
-import { use } from "react";
-import "./PokemonCard.scss";
-import { CheckboxGroup, Checkbox } from "rsuite";
-import { useState, useEffect } from "react";
+import './PokemonCard.scss';
+import { Checkbox } from 'rsuite';
+import { useState } from 'react';
 
-const PokemonCard = ({ pokemon, pokemonArray }) => {
+// When a user selects a pokemon -> we want to add that pokemon object to the pokemon array (state)
+// We update the favourites pokemons array on the backend when the user  clicks on a Confirm button
 
-  //const [isChecked, setIsChecked] = useState(!isChecked);
+const PokemonCard = ({ pokemon, onSelectPokemon, onUnselectPokemon }) => {
+    const [isChecked, setIsChecked] = useState(false);
+    const handleTickCheckboxChange = () => {
+        console.log(isChecked);
+        if (!isChecked) {
+            onSelectPokemon(pokemon);
+            setIsChecked(true);
+        } else {
+            setIsChecked(false);
+            onUnselectPokemon(pokemon);
+        }
+    };
 
-  
-  
-	const onChangeHandler = (e) => {
-		console.log("Selected Pokemon:", pokemon.name);
+    return (
+        <div className="pokemon-card">
+            <section className="pokemon-card_header">
+                <p>{pokemon.id}</p>
+                <h1 className="pokemon-card_name">{pokemon.name}</h1>
+            </section>
 
-    setIsChecked(!isChecked);
-    pokemon.isChecked = !isChecked;
-    console.log(pokemonArray)
-    // if (!isChecked) {
-    //   pokemonArray.push(pokemon); 
-    // } else {
-    //   pokemonArray = pokemonArray.filter(p => p.id !== pokemon.id);
-    // }
-    
-    console.log(pokemon)
-	};
-    
+            <img
+                className="pokemon-card_image"
+                src={pokemon.sprites.other['official-artwork'].front_default}
+                alt={pokemon.name}
+            />
 
-  // useEffect(() => {
-  //   if (isChecked) {
-  //     pokemonArray.push(pokemon); 
-  //   }
-  // }, []);
-  
+            <section className="pokemon-card_info">
+                <p>
+                    <strong>
+                        Ability: {pokemon.abilities[0].ability.name}{' '}
+                    </strong>
+                </p>
 
-	return (
-		<section className="pokemon-card">
-			<section className="pokemon-card_header">
-				<p>{pokemon.id}</p>
-				<h1 className="pokemon-card_name">{pokemon.name}</h1>
-				<div className="App">
-					<Checkbox
-						name="Pokemon"
-						inline
-						onChange={onChangeHandler}
-						value={pokemon}>
-            
-          </Checkbox>
-            
-				</div>
-			</section>
-      
+                <p>
+                    <strong>Base Experience : {pokemon.base_experience}</strong>
+                </p>
+                <p>
+                    <strong>Height: {pokemon.height}</strong>
+                </p>
+                <p>
+                    <strong>Weight: {pokemon.weight}kg</strong>
+                </p>
+                {pokemon.types.map((oneType) => (
+                    <p key={oneType.type.name}>
+                        <strong>
+                            Type {oneType.slot}: {oneType.type.name}
+                        </strong>
+                    </p>
+                ))}
+            </section>
 
-			<img
-				className="pokemon-card_image"
-				src={pokemon.sprites.other["official-artwork"].front_default}
-				alt={pokemon.name}
-			/>
-			<section className="pokemon-card_info">
-				<p>
-					<strong>
-						Ability: {pokemon.abilities[0].ability.name}{" "}
-					</strong>
-				</p>
-        <p>{isChecked ? "Checked" : "Unchecked"}</p>
-				<p>
-					<strong>Base Experience : {pokemon.base_experience}</strong>
-				</p>
-				<p>
-					<strong>Height: {pokemon.height}</strong>
-				</p>
-				<p>
-					<strong>Weight: {pokemon.weight}kg</strong>
-				</p>
-        {pokemon.types.map((oneType) => (
-          <p key={oneType.type.name}>
-            <strong>Type {oneType.slot}: {oneType.type.name}</strong>
-          </p>
-        ))}
-			</section>
-		</section>
-	);
+            <Checkbox
+                name="Pokemon"
+                checked={pokemon.isFavourite}
+                onChange={handleTickCheckboxChange}
+                value={pokemon}></Checkbox>
+        </div>
+    );
 };
 
 export default PokemonCard;
-
--- frontend/src/containers/Dashboard/Dashboard.jsx
+++ frontend/src/containers/Dashboard/Dashboard.jsx
@@ -1,31 +1,54 @@
-import PokemonButtons from "../../components/PokemonButtons/PokemonButtons";
-import PokemonCard from "../../components/PokemonCard/PokemonCard";
-import { getOnlyUrl } from "../../functions";
-import "./Dashboard.scss";
-import { useState } from "react";
+import PokemonButtons from '../../components/PokemonButtons/PokemonButtons';
+import { useState } from 'react';
+import PokemonCard from '../../components/PokemonCard/PokemonCard';
+import './Dashboard.scss';
 
+const Dashboard = ({ pokemonData }) => {
+    const [selectedPokemon, setSelectedPokemon] = useState([]);
+    console.log(selectedPokemon);
+    const [favouritePokemons, setFavouritePokemons] = useState([]);
 
+    // Add a key on pokemon object to identify the pokemon which are already in the favourites list
 
-const Dashboard = ({pokemonData}) => {
-  
-  let pokemonArray = [];
-  if (pokemonData) {
-    pokemonData.forEach(pokemon => {
-      if (pokemon.isChecked) {  
-        pokemonArray.push(pokemon);
-      } 
-    })
-  };
-  
-  
+    const updatedPokemonData = pokemonData.map((pokemon) => {
+        return {
+            ...pokemon,
+            isFavourite: favouritePokemons.includes(pokemon.id),
+        };
+    });
 
-  return (
-    <div className="Dashboard">
-        {pokemonData? pokemonData.map((pokemon) => {
-          return <PokemonCard pokemonArray={pokemonArray} key={pokemon.id} pokemon={pokemon}/>
-        }) : <p>Loading...</p>}
-    </div>
-  )
-}
+    // Create a submit button to send the selected pokemons to the backend
+    // you need to then trigger a refresh of the dashboard to show the new favourites
+    // Do API call to favouritePokemons endpoint to check which pokemons are already in the favourites list
+    // Render the pokemons cards with the checkbox checked if the pokemon is in the favourites list
 
-export default Dashboard
+    const handleSelectPokemon = (pokemon) => {
+        setSelectedPokemon([...selectedPokemon, pokemon]);
+    };
+
+    const handleUnselectPokemon = (pokemon) => {
+        setSelectedPokemon(selectedPokemon.filter((p) => p.id !== pokemon.id));
+    };
+
+    return (
+        <div className="Dashboard">
+            {pokemonData ? (
+                pokemonData.map((pokemon) => {
+                    return (
+                        <PokemonCard
+                            pokemonArray={pokemonData}
+                            key={pokemon.id}
+                            pokemon={pokemon}
+                            onSelectPokemon={handleSelectPokemon}
+                            onUnselectPokemon={handleUnselectPokemon}
+                        />
+                    );
+                })
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default Dashboard;
